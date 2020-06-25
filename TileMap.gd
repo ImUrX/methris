df53ed1ef04c619bf8c3f01524bc40876d9606c3
Mateo Cornejo <mateoh.cornejo@gmail.com>
2020-06-25 14:18:43 -0300

    Z bloque

--- TileMap.gd
+++ TileMap.gd
@@ -3,7 +3,7 @@ extends TileMap
 
 const xMax = [10,19]
 const yMax = [0,19]
-var blocks = [load("res://blocks/Stick.gd"), load("res://blocks/J.gd"), load("res://blocks/S.gd")]
+var blocks = [load("res://blocks/Stick.gd"), load("res://blocks/J.gd"), load("res://blocks/S.gd"), load("res://blocks/Z.gd")]
 
 # Declare member variables here. Examples:
 var time = 0
