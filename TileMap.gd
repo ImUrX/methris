9567de27fc3e65402538374555e91bd24cf340bf
roviet-sussia <lautvillada@gmail.com>
2020-08-13 11:09:05 -0300

    oops

--- TileMap.gd
+++ TileMap.gd
@@ -9,6 +9,7 @@ var time = 0
 var rng = RandomNumberGenerator.new()
 var controls = load("res://Controls.gd").new()
 var instance
+var score
 # var b = "text"
 
 signal fullLineDone(y)
@@ -83,8 +84,6 @@ func fullLine(y: int):
 		if not (self.get_cell(x, y) >= 3): return false
 	return true
 	
-func addScore(num: int):
-	var sum = 0
-	for _i in range(0, num):
-		sum = (sum + 100) + (sum + 100) * 0.2 * num
-	return sum
+func addScore():
+	
+	return
