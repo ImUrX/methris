06f1e47dd6be664e5523bcb89a992a17e3b71969
ImUrX <urielfontan2002@gmail.com>
2020-10-15 14:28:48 -0300

    add music

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -17,3 +17,17 @@ func _ready():
 
 func _on_TextureButton_pressed():
 	get_tree().change_scene("res://Game.tscn")
+
+
+func _on_OptionsMenuButton_pressed():
+	$MainMenu.visible = false
+	$Options.visible = true
+
+
+func _on_HSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
+
+
+func _on_BackButton_pressed():
+	$MainMenu.visible = true
+	$Options.visible = false
